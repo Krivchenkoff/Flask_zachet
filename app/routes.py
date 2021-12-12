cc3d5fba95062c27ac242647340d8e1f7561396b
root <root@localhost.localdomain>
2021-12-13 00:25:37 +0300

    free_lesson_first_login_view

--- app/routes.py
+++ app/routes.py
@@ -1,22 +1,10 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
-@app.route('/')
-@app.route('/index')
-def index():
-    user = {'username': 'Stepan Krivchenkov'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }, 
-        {
-            'author': {'username': 'Ippolit'},
-            'body': 'Kakaya gadost vasha zalivnaya ryba!!'
-        }
-    ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+# ...
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
