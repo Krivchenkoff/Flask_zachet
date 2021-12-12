e367b2815b5f020cb4981e6b56c29039b5ce72b1
root <root@localhost.localdomain>
2021-12-12 23:06:23 +0300

    second_fucking_lesson_part_free

--- app/routes.py
+++ app/routes.py
@@ -4,5 +4,19 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Stepan'}
-    return render_template('index.html', title='Home', user=user)
+    user = {'username': 'Stepan Krivchenkov'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }, 
+        {
+            'author': {'username': 'Ippolit'},
+            'body': 'Kakaya gadost vasha zalivnaya ryba!!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
